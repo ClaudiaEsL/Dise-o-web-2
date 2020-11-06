dbe470d3dcc3b677c098f06fbd44a1a10e947c43
ClaudiaEsL <abiC.CAEL@gmail.com>
2020-11-06 16:37:31 -0400

    commit

--- js/fecha.js
+++
@@ -1,8 +0,0 @@
-/*Obtener la fecha actual */
-var fecha_actual = new Date();
-/*Fecha*/
-var day = fecha_actual.getDay();
-var month = fecha_actual.getMonth();
-var year = fecha_actual.getFullYear();
-
-document.write(' Fecha: '+day+'/'+month+'/'+year);
