720edad21411081124087dabe3d6a418faeec2b8
ClaudiaEsL <abiC.CAEL@gmail.com>
2020-11-15 16:49:37 -0400

    commit

--- Practica3/src/app/app.module.ts
+++ Practica3/src/app/app.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//Importamos mi componente
-import { MiComponente } from './Componentes/Componente1/Mi-Componente';
+
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { CuerpoComponent } from './cuerpo/cuerpo.component';
 import { PieComponent } from './pie/pie.component';
@@ -12,7 +11,6 @@ import { PieComponent } from './pie/pie.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponente,
     CabeceraComponent,
     CuerpoComponent,
     PieComponent
